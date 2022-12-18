caa18ca35be1bd19fd8ba652b1b9a774ffaadd1f
reenadgh <miss_roreee@hotmail.com>
2022-12-18 22:43:46 +0300

    this commit from iTerm !

---
+++ Project-1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project-1
+//
+//  Created by Reenad gh on 24/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Hello, world !")
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .cornerRadius(20)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-1/Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1App.swift
+//  Project-1
+//
+//  Created by Reenad gh on 24/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
